7851b912d4bd5ccdfcbcea56a2a28ccfb3e5e5fb
Arthur Ribeiro <arthhur.ribeiro@gmail.com>
2022-05-17 19:02:44 -0300

    On mouse drag

--- Assets/Sripts/Bird.cs
+++ Assets/Sripts/Bird.cs
@@ -14,4 +14,11 @@ public class Bird : MonoBehaviour
     {
         GetComponent<SpriteRenderer>().color = Color.white;
     }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, newPosition.y);
+
+    }
 }
