52710cfc9d3a522e8fb902358b9062f9ed2c1481
Camila Bortolotti <camilagbortolotti@gmail.com>
2022-05-18 19:21:34 -0300

    Launching add force

--- Assets/Sripts/Bird.cs
+++ Assets/Sripts/Bird.cs
@@ -2,14 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 {
     private Vector3 _initialPosition;
-    
+    private bool _birdWasLaunched;
+    private float _timiSittinAround;
+
+    [SerializeField] private float _launchPower = 600;
+
     private void Awake()
     {
         _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (_birdWasLaunched &&
+            GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1f)
+        {
+            _timiSittinAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 || transform.position.y < -10 ||
+            transform.position.x > 10 || transform.position.x < -10 ||
+            _timiSittinAround > 3)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
     }
 
     private void OnMouseDown()
@@ -20,6 +42,13 @@ public class Bird : MonoBehaviour
     private void OnMouseUp()
     {
         GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+
+        _birdWasLaunched = true;
     }
 
     private void OnMouseDrag()
